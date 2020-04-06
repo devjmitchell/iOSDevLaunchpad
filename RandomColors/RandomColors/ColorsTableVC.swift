575fe9d3bdbfd19d8cb51e86052d3ff73f961b58
Jason Mitchell <devjmitchell@gmail.com>
2020-04-06 08:02:39 -0700

    UITableView setup

--- RandomColors/RandomColors/ColorsTableVC.swift
+++ RandomColors/RandomColors/ColorsTableVC.swift
@@ -12,22 +12,23 @@ class ColorsTableVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
+
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
