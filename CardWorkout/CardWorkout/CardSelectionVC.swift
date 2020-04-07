0bb2a88e78df5f187d510ce46a87200fb6a6573e
Jason Mitchell <devjmitchell@gmail.com>
2020-04-07 09:09:39 -0700

    Set up storyboard UI

---
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -0,0 +1,30 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout
+//
+//  Created by Jason Mitchell on 4/7/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
