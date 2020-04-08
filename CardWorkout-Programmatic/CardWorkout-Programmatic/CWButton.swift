1158b1d354f2ec16a478fdfe7fed055cd8be804d
Jason Mitchell <devjmitchell@gmail.com>
2020-04-08 09:08:56 -0700

    Create custom UIButton

---
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CWButton.swift
@@ -0,0 +1,37 @@
+//
+//  CWButton.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Jason Mitchell on 4/8/20.
+//  Copyright © 2020 Jason Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
+    
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
