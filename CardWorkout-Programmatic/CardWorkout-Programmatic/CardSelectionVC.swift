4a2eae51cec8039d53853407473d7d9ea3bf3c37
Jason Mitchell <devjmitchell@gmail.com>
2020-04-08 12:39:58 -0700

    Finish programmatic app by adding logic

--- CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -14,11 +14,36 @@ class CardSelectionVC: UIViewController {
     let stopButton = CWButton(backgroundColor: .systemRed, title: "Stop!")
     let resetButton = CWButton(backgroundColor: .systemGreen, title: "Reset")
     let rulesButton = CWButton(backgroundColor: .systemBlue, title: "Rules")
+    
+    var cards: [UIImage] = CardDeck.allValues
+    var timer: Timer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureUI()
+        startTimer()
+    }
+    
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomCard), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func stopTimer() {
+        timer.invalidate()
+    }
+    
+    
+    @objc func resetTimer() {
+        stopTimer()
+        startTimer()
+    }
+    
+    
+    @objc func showRandomCard() {
+        cardImageView.image = cards.randomElement()
     }
     
     
@@ -46,6 +71,7 @@ class CardSelectionVC: UIViewController {
     
     func configureStopButton() {
         view.addSubview(stopButton)
+        stopButton.addTarget(self, action: #selector(stopTimer), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             stopButton.widthAnchor.constraint(equalToConstant: 260),
@@ -58,6 +84,7 @@ class CardSelectionVC: UIViewController {
     
     func configureResetButton() {
         view.addSubview(resetButton)
+        resetButton.addTarget(self, action: #selector(resetTimer), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             resetButton.widthAnchor.constraint(equalToConstant: 115),
